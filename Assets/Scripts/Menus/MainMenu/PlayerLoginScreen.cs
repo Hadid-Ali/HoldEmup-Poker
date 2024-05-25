ab1ba807c83a5d64f76bcc816d703cfc15ff3bcc
Rachit Chahar <202151123@iiitvadodara.ac.in>
2024-05-25 12:01:19 +0530

    feat: add create-player-account endpoint

--- Assets/Scripts/Menus/MainMenu/PlayerLoginScreen.cs
+++ Assets/Scripts/Menus/MainMenu/PlayerLoginScreen.cs
@@ -5,11 +5,24 @@ using TMPro;
 using UnityAtoms.BaseAtoms;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 
 public class PlayerLoginScreen : UIMenuBase
 {
     [SerializeField] private TMP_InputField m_InputField;
     [SerializeField] private Button m_LoginButton;
+
+    [System.Serializable]
+    public class PlayerAccountCreationRequest
+    {
+        public string username;
+    }
+
+    [System.Serializable]
+    public class PlayerAccountCreationResponse
+    {
+        public ulong account_id;
+    }
     
     private void Start()
     {
@@ -53,8 +66,45 @@ public class PlayerLoginScreen : UIMenuBase
 
     private void LoginInternal()
     {
+        // create player account /v1/player/create-account here
+        PlayerAccountCreationRequest request = new PlayerAccountCreationRequest
+        {
+            username = GameData.RuntimeData.USER_NAME
+        };
+        StartCoroutine(CreatePlayerAccount(request));
+
         GameEvents.MenuEvents.LoginAtMenuEvent.Raise(GameData.RuntimeData.USER_NAME);
         ChangeMenuState(MenuName.ConnectionScreen);
+    }
+
+    private IEnumerator CreatePlayerAccount(PlayerAccountCreationRequest request)
+    {
+        Debug.Log("Creating player account...");
+        string url = "http://127.0.0.1:8000/v1/player/create-account";
+        string jsonData = JsonUtility.ToJson(request);
+
+        using (UnityWebRequest webRequest = new UnityWebRequest(url, "POST"))
+        {
+            byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+            webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
+            webRequest.downloadHandler = new DownloadHandlerBuffer();
+            webRequest.SetRequestHeader("Content-Type", "application/json");
+
+            Debug.Log("Sending account creation request...");
+
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError || webRequest.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("Error: " + webRequest.error);
+            }
+            else
+            {
+                string responseText = webRequest.downloadHandler.text;
+                PlayerAccountCreationResponse response = JsonUtility.FromJson<PlayerAccountCreationResponse>(responseText);
+                Debug.Log("Account ID: " + response.account_id);
+            }
+        }
     }
 
     private void SetButtonInteractionStatus(bool status)
